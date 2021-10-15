a4d7bd64d447f4a57f28c14f67b48c9b2ddc628a
irviariefatul <irviariefatul10@gmail.com>
2021-10-15 11:15:15 +0700

    Pembuatan tabel students

---
+++ database/seeders/StudentSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class StudentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('students')->insert([
+            'nim' => '2031710036',
+            'name' => 'Irvi Ariefatul Julia Putri',
+            'class' => 'MI-2E',
+            'department' => 'JTI',
+            'phone_number' => '081335098272',
+        ]);
+
+        DB::table('students')->insert([
+            'nim' => '2031710044',
+            'name' => 'Luvi Anggraini',
+            'class' => 'MI-2E',
+            'department' => 'JTI',
+            'phone_number' => '081335098272',
+        ]);
+
+        DB::table('students')->insert([
+            'nim' => '2031710006',
+            'name' => 'Mareta Rosa Anjellina',
+            'class' => 'MI-2E',
+            'department' => 'JTI',
+            'phone_number' => '081335098272',
+        ]);
+    }
+}
